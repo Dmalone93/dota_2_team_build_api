528166e1e07c410ef1b2331ebb12b4f56876149f
Declan Malone <declanamalone@gmail.com>
2019-01-13 19:57:58 +0000

    tried looping through roles to insert into div

--- src/views/dota_detail_view.js
+++ src/views/dota_detail_view.js
@@ -10,33 +10,42 @@ const DotaDetailView = function(container){
 DotaDetailView.prototype.bindEvents = function(){
   PubSub.subscribe('DotaListView:character-selected', (event) => {
     this.renderCharacterName(event.detail)
+
   })
   PubSub.subscribe('SelectView:player-roles-ready', (event) => {
-    this.renderCharacterRole(event.detail)
+    this.renderCharacterRole(event.target.value)
     console.log(event.detail);
   })
 }
 
 
 DotaDetailView.prototype.renderCharacterName = function(character){
-  const characterObject = JSON.parse(character)
-  console.log(characterObject);
+  const characterObject = JSON.parse(character);
   const playerDiv = document.createElement('div');
   const header = document.createElement('h2');
   header.textContent = characterObject.localized_name
   console.log(header);
   playerDiv.appendChild(header);
-
-  const playerRole = document.createElement('h3');
-  playerRole.textContent = characterObject.roles
-  console.log(playerRole);
-  playerDiv.appendChild(playerRole);
+  console.log(playerDiv);
 
 };
 
 DotaDetailView.prototype.renderCharacterRole = function(role){
-
+  character.forEach((characterRole) => {
+    const playerRole = document.createElement('h3')
+    playerRole.value = characterRole
+    playerRole.textContent = characterRole.roles
+    console.log(playerRole);
+    playerDiv.appendChild(playerRole);
+  });
 
 
 };
+
+
+
+
+
+
+
 module.exports = DotaDetailView;
